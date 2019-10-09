f6fd19857cd035232dabccabe1e88d60df5cab34
Robert Freeman <robertisafreeman@gmail.com>
2019-10-09 12:55:20 -0600

    stuck in a funk

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
-const _Home = ({students}) => 
-        <div>
-            Home - there are {students.length} students!
-            <hr/>
-        </div>
-;
 
+const _Home = ({students}) => <div>
+    <h1>
+        Home
+    </h1>
+    <hr/>
+    <div>
+        Our most popular school is The Clown College with 100 students!
+    </div>
+    <hr/>
+    <div>
+        Our top performing school is The School for Dogs with an average GPA of 4.6!
+    </div>
+    <hr/>
+    </div>
+;
 const Home = connect(({students})=> {
     return {
         students
